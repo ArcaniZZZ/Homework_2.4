954514151b84f9b5c707a748f3019b9e92003412
Alex <arcani91@gmail.com>
2021-08-27 22:39:36 +0300

    Button styles are set up!

--- Homework_2.4/LoginPageViewController.swift
+++ Homework_2.4/LoginPageViewController.swift
@@ -10,24 +10,50 @@ import UIKit
 class LoginPageViewController: UIViewController {
     
     @IBOutlet var letsStartButton: UIButton!
+    @IBOutlet var remindUsernameButton: UIButton!
+    @IBOutlet var remindPasswordButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        makeTheButtonNice(button: letsStartButton)
     }
     
-    func ViewWillLayoutSubview() {
-        makeTheButtonNice(button: letsStartButton)
+    override func viewDidLayoutSubviews() {
+        makeTheButtonNice(buttons:
+                            letsStartButton,
+                          remindUsernameButton,
+                          remindPasswordButton)
     }
 }
 
-
 extension LoginPageViewController {
     
-    func makeTheButtonNice(button: UIButton) {
-        button.layer.borderWidth = 1
-        button.layer.cornerRadius = 10
-        button.layer.backgroundColor = UIColor.systemBlue.cgColor
+    func makeTheButtonNice(buttons: UIButton...) {
+        
+        let textColor = UIColor(cgColor: CGColor(red: 255,
+                                                  green: 255,
+                                                  blue: 255,
+                                                  alpha: 1))
+        
+        let shadowColor = UIColor(cgColor: CGColor(red: 0,
+                                                  green: 0,
+                                                  blue: 0,
+                                                  alpha: 0.5))
+        
+        let contentInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+        
+        for button in buttons {
+            button.layer.borderWidth = 0.5
+            button.layer.cornerRadius = 5
+            button.layer.backgroundColor = CGColor(red: 0,
+                                                   green: 0,
+                                                   blue: 0,
+                                                   alpha: 0.6)
+            button.setTitleColor(textColor, for: .normal)
+            button.setTitleShadowColor(shadowColor, for: .normal)
+            button.contentEdgeInsets = contentInsets
+            button.titleLabel?.minimumScaleFactor = 0.5
+            button.titleLabel?.adjustsFontSizeToFitWidth = true
+        }
     }
 }
 
