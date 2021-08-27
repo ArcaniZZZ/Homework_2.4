0eea5ceb60b7779f1a8db755a36d0f77e42ced5d
Alex <arcani91@gmail.com>
2021-08-27 18:49:15 +0300

    Initial Commit

---
+++ Homework_2.4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_2.4
+//
+//  Created by Arcani on 27.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
