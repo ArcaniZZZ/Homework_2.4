b30a1f411d53bd80cd7e5cd1bca09f307331048f
Alex <arcani91@gmail.com>
2021-08-27 21:15:58 +0300

    Setting up the button design.

--- Homework_2.4/LoginPageViewController.swift
+++ Homework_2.4/LoginPageViewController.swift
@@ -8,11 +8,28 @@
 import UIKit
 
 class LoginPageViewController: UIViewController {
-
+    
+    @IBOutlet var letsStartButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        makeTheButtonNice(button: letsStartButton)
     }
-
-
+    
+    func ViewWillLayoutSubview() {
+        makeTheButtonNice(button: letsStartButton)
+    }
 }
+
+
+extension LoginPageViewController {
+    
+    func makeTheButtonNice(button: UIButton) {
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 10
+        button.layer.backgroundColor = UIColor.systemBlue.cgColor
+    }
+}
+
+
 
