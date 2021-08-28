ddafad79dd6c30a00ba96fddcea92a4aad0ee45c
Alex <arcani91@gmail.com>
2021-08-28 19:04:43 +0300

    Finished preparations!

--- Homework_2.4/GreetingScreenViewController.swift
+++ Homework_2.4/GreetingScreenViewController.swift
@@ -9,8 +9,14 @@ import UIKit
 
 class GreetingScreenViewController: UIViewController {
 
+    @IBOutlet var greetingUserLabel: UILabel!
+    
+    var username = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        super.viewDidLoad()
+        greetingUserLabel.text = "Hello, \(username)!"
     }
 }
--- Homework_2.4/LoginPageViewController.swift
+++ Homework_2.4/LoginPageViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class LoginPageViewController: UIViewController {
     
+    // MARK: - IB Outlets
     @IBOutlet var loginField: UITextField!
     @IBOutlet var passwordField: UITextField!
     
@@ -16,13 +17,11 @@ class LoginPageViewController: UIViewController {
     @IBOutlet var remindUsernameButton: UIButton!
     @IBOutlet var remindPasswordButton: UIButton!
     
-    let password = "Password"
-    let login = "Login"
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
+    // MARK: - Private Properties
+    private let password = "Password"
+    private let login = "Username"
+        
+    // MARK: - Overriden functions
     override func viewDidLayoutSubviews() {
         makeTheButtonNice(buttons:
                             letsStartButton,
@@ -30,8 +29,18 @@ class LoginPageViewController: UIViewController {
                           remindPasswordButton)
     }
     
-    @IBAction func logInButton() {
-        guard loginField.text == login && passwordField.text == password
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let loggedInVc = segue.destination as? GreetingScreenViewController
+        else {
+            return
+        }
+        loggedInVc.username = login
+    }
+    
+    // MARK: - IB Actions
+    
+    @IBAction func loginButtonPressed() {
+            guard loginField.text == login && passwordField.text == password
         else {
             wrongEntry()
             return
@@ -49,30 +58,26 @@ class LoginPageViewController: UIViewController {
     }
     
     @IBAction func unwind(for unwindSegue: UIStoryboardSegue) {
+        loginField.text = nil
+        passwordField.text = nil
     }
-    
 }
 
+// MARK: - Extension for button style
 extension LoginPageViewController {
-    
     func makeTheButtonNice(buttons: UIButton...) {
-        
         let textColor = UIColor(cgColor: CGColor(red: 255,
                                                  green: 255,
                                                  blue: 255,
                                                  alpha: 1))
-        
         let shadowColor = UIColor(cgColor: CGColor(red: 0,
                                                    green: 0,
                                                    blue: 0,
                                                    alpha: 0.5))
-        
         let contentInsets = UIEdgeInsets(top: 0,
                                          left: 10,
                                          bottom: 0,
                                          right: 10)
-        
-        
         for button in buttons {
             button.layer.borderWidth = 0.5
             button.layer.cornerRadius = 5
@@ -80,7 +85,6 @@ extension LoginPageViewController {
                                                    green: 0,
                                                    blue: 0,
                                                    alpha: 0.6)
-            
             button.setTitleColor(textColor, for: .normal)
             button.setTitleShadowColor(shadowColor, for: .normal)
             button.contentEdgeInsets = contentInsets
@@ -90,27 +94,22 @@ extension LoginPageViewController {
     }
 }
 
+// MARK: - Extension for alerts
 extension LoginPageViewController {
-    
     private func reminder(title: String, message: String) {
         let reminder = UIAlertController(title: title,
                                          message: message,
                                          preferredStyle: .alert)
-        
         let okAction = UIAlertAction(title: "Oh me!",
                                      style: .default)
-        
         reminder.addAction(okAction)
         present(reminder, animated: true)
     }
-    
     private func wrongEntry(title: String = "Oops!",
                             message: String = "Incorrect password or login!") {
-        
         let alert = UIAlertController(title: title,
                                       message: message,
                                       preferredStyle: .alert)
-        
         let okAction = UIAlertAction(title: "Silly goose!",
                                      style: .default,
                                      handler: { _ in
@@ -118,7 +117,21 @@ extension LoginPageViewController {
         alert.addAction(okAction)
         present(alert, animated: true)
     }
-    
 }
 
-
+// MARK: - Extension for UITextFieldDelegate
+extension LoginPageViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == loginField {
+            passwordField.becomeFirstResponder()
+        } else {
+            loginButtonPressed()
+            performSegue(withIdentifier: "showWelcomeVC", sender: nil)
+        }
+        return true
+    }
+}
